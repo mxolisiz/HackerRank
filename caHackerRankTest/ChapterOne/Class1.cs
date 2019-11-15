486a8e69f52713f529323c19911c7e3519f8471b
Mxolisi Zondi (absa) <Mxolisi.Zondi@absa.africa>
2019-11-15 08:40:54 +0200

    Palindrome
    Is Palindrome functional solve & Test

---
+++ caHackerRankTest/ChapterOne/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace caMcsdPractice.ChapterOne
+{
+    public class Class1
+    {
+        static bool IsPalindrome(string word)
+        {
+            string testWord = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+                testWord += word[i];
+
+
+            if (testWord.Equals(word))
+                return true;
+
+            return false;
+        }
+
+        static bool IsPalindromeFunc(string word)
+        {
+            string testWord = "";
+            word.Split(new string[] {""}, StringSplitOptions.None).ToList().ForEach(l => {
+                testWord += l;
+            });
+
+            if (testWord.Equals(word))
+                return true;
+
+            return false;
+        }
+
+        static bool IsPalindromeFunctional(string word)
+        {
+            string testWord = "";
+            int counter = word.Length;
+
+            while (word.Length != testWord.Length)
+            {
+                testWord += word[counter];
+                counter--;
+            }
+
+            if (testWord.Equals(word))
+                return true;
+
+            return false;
+        }
+
+        public bool IsPalindromeSuperFunctional(string word)
+        {
+            string testWord = "";
+
+            while (word.Length != testWord.Length)
+            {
+                testWord += word[GetIndex()];
+                Console.WriteLine($"{testWord}: {GetIndex()}");
+            }
+            Console.WriteLine(testWord);
+            return testWord == word;
+
+
+            int GetIndex() => (word.Length - 1) - testWord.Length;
+        }
+    }
+}
--- mcsdUnitTest/ChapterOne/UnitTest1.cs
+++ mcsdUnitTest/ChapterOne/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace caMcsdPractice.ChapterOne.Tests
     [TestClass()]
     public class UnitTest1
     {
-        [TestMethod()]
+        [TestMethod]
         public void SwitchAndStuffTest()
         {
             //Arrange
@@ -16,6 +16,17 @@ namespace caMcsdPractice.ChapterOne.Tests
             byte returnVar=ch1.SwitchAndStuff(chapterone.mxolisi.excited);
             //Assert
             Assert.AreEqual((byte)chapterone.mxolisi.excited, returnVar);
+        }
+
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        public void TestIsPalindrome_ShouldReturnTrue()
+        {
+            //Arrange
+            Class1 mxolisi = new Class1();
+            //Act
+            var result=mxolisi.IsPalindromeSuperFunctional("kayak");
+            //Assert
+            Assert.IsTrue(result);
         }
     }
 }
