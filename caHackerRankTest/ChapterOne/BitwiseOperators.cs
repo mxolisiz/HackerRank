29bb7c654855a9687ddd7aa193c013e6ed4b107a
Mxolisi Zondi <44498385+mxolisiz@users.noreply.github.com>
2019-11-17 18:20:21 +0200

    Create BitwiseOperators.cs

---
+++ caHackerRankTest/ChapterOne/BitwiseOperators.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace caMcsdPractice.ChapterOne
+{
+    public class BitwiseOperators
+    {
+        public void UnaryVariantOperator()
+        {
+            bool isBest = false;
+            bool isValue = true;
+            if (isBest ^ isValue)//if one of the two is true
+                Debug.WriteLine($"isBest:{isBest} & isValue:{isValue}");
+            if (isBest && isValue)
+                Debug.WriteLine($"isBest:{isBest} && isValue:{isValue}");
+
+            short mxolisi = ~3;
+            bool val = false;
+            Debug.WriteLine(Convert.ToString(mxolisi, 2));//1111111111111100
+
+        }
+    }
+}
